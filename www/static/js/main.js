b4d82263a7249c6296a9232ca40f23a1afaa52e3
Vazha Omanashvili <rainum.ua@gmail.com>
2017-03-26 12:33:34 +0300

    front 1

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,4 +1,6 @@
 (function($) {
+  var doctorId;
+
   function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -38,36 +40,38 @@
   $("#phone").mask("+380 (099) 999-99-99");
 
   var thisMonth = moment().format('YYYY-MM');
+  var thisDay = moment().format('YYYY-MM-DD');
+
+  moment.locale('ru');
 
   $('.calendar').clndr({
     events: [
       /*{
-        title: 'Multi-Day Event',
-        endDate: thisMonth + '-14',
-        startDate: thisMonth + '-10'
-      }, {
-        endDate: thisMonth + '-23',
-        startDate: thisMonth + '-21',
-        title: 'Another Multi-Day Event'
-      }, {
-        date: thisMonth + '-27',
-        title: 'Single Day Event'
-      }*/
+       title: 'Multi-Day Event',
+       endDate: thisMonth + '-14',
+       startDate: thisMonth + '-10'
+       }, {
+       endDate: thisMonth + '-23',
+       startDate: thisMonth + '-21',
+       title: 'Another Multi-Day Event'
+       }, {
+       date: thisMonth + '-27',
+       title: 'Single Day Event'
+       }*/
     ],
     clickEvents: {
       click: function(target) {
-        console.log('Cal-1 clicked: ', target.date.format('YYYY-MM-DD'));
+        var date = target.date.format('YYYY-MM-DD');
+
+        if (moment(date).isSameOrAfter(thisDay)) {
+          console.log(date);
+
+          getFreeSlots(date, doctorId);
+        }
       }
     },
-    multiDayEvents: {
-      singleDay: 'date',
-      endDate: 'endDate',
-      startDate: 'startDate'
-    },
-    showAdjacentMonths: true,
-    adjacentDaysChangeMonth: false,
     constraints: {
-      startDate: moment().format('YYYY-MM-DD') // Limit calendar by current day
+      startDate: thisDay // Limit calendar by current day
     }
   });
 
@@ -75,6 +79,44 @@
     $('.step').hide(0);
     $('.step-' + index).show(0);
   }
+
+  function getFreeSlots(date, id) {
+    $('.time-container .loading').fadeIn(300);
+
+    $.ajax({
+      method: 'POST',
+      url: '/static/free-slots.json',
+      dataType: 'json',
+      data: {
+        doctorId: id,
+        date: date
+      }
+    }).done(function(data) {
+      var slotsHtml = data.map(function(slot) {
+        if (slot.available) {
+          return '<tr><td>' + slot.time + '</td><td><button class="btn btn-small btn-primary btn-pick-time">Обрати</button></td></tr>';
+        }
+
+        return '<tr class="active"><td>' + slot.time + '</td><td></td></tr>';
+      });
+
+      $('.time-container .table tbody').html(slotsHtml);
+
+      $('.time-container .loading').fadeOut(300);
+    });
+  }
+
+  $('.time-container').on('click', '.btn-pick-time', function(e) {
+    e.preventDefault();
+    $('.time-container .table tr').removeClass('success');
+
+    var row = $(this).closest('tr');
+    var time = row.find('td:first').text();
+
+    row.addClass('success');
+
+    console.log(time);
+  });
 
   $('.prev-step').on('click', function(e) {
     e.preventDefault();
@@ -90,18 +132,28 @@
       return acc;
     }, {});
 
-    $('.loading').fadeIn(300);
+    $(this).find('.loading').fadeIn(300);
 
     $.ajax({
       method: 'POST',
       url: '/select-address',
       dataType: 'json',
       data: dataObj
-    }).done(function() {
-      console.log("success");
+    }).done(function(data) {
+      var doctor = data.doctor;
+
+      doctorId = doctor.id;
+
+      $('.doctor-name').text([
+        doctor.last_name,
+        doctor.first_name,
+        doctor.second_name
+      ].join(' '));
+
+      getFreeSlots(thisDay, doctorId);
+      showStep(2);
     }).always(function() {
       $('.loading').fadeOut(300);
-      showStep(2);
     });
 
     return false;
