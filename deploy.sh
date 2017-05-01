1f7ab6cc4d4c08db737f28465f612e02c8686c19
FerumFlex <ferumflex@gmail.com>
2017-05-01 12:19:12 +0700

    swarm

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -e
 git checkout master
 
 # docker build -t ferumflex/edoctor . && docker push ferumflex/edoctor
+export DOCKER_HOST=tcp://ferumflex.hopto.org:2376 DOCKER_TLS_VERIFY=1
 docker build -t registry.gitlab.com/ferumflex/edoctor . && docker push registry.gitlab.com/ferumflex/edoctor
 
 now=$(date +"%Y/%m/%d")
@@ -30,4 +31,5 @@ git push origin "$tag"
 git push gitlab master
 git push gitlab "$tag"
 
-docker-cloud service redeploy edoctor-web
+export DOCKER_HOST=tcp://127.0.0.1:32768 DOCKER_TLS_VERIFY=
+docker stack deploy -c docker-swarm.yml edoctor --with-registry-auth --prune
